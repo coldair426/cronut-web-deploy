09d7cf4a8f3bc8b1d556b6fe349e35c28250bbda
coldair426 <coldair426@gmail.com>
2025-04-02 01:56:20 +0000

    회사 위치 변경(강촌, 을지) 컴포넌트화(CompanySelect) => 메인, 장바구니 생성 화면에 적용

--- src/context/CompanyContext.tsx
+++ src/context/CompanyContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useMemo, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useMemo, useState } from 'react';
 import { Company } from '@/types/common';
 
 interface CompanyContextType {
@@ -21,7 +21,23 @@ export const CompanyProvider = ({ children }: { children: ReactNode }) => {
         (localStorage.getItem('recentCompany') as Company) || Company.KANGCHON
     );
 
-    const contextValue = useMemo(() => ({ company, setCompany }), [company]);
+    useEffect(() => {
+        const storedCompany = localStorage.getItem('recentCompany');
+
+        if (storedCompany && Object.values(Company).includes(storedCompany as Company)) {
+            setCompany(storedCompany as Company);
+        } else {
+            localStorage.setItem('recentCompany', Company.KANGCHON); // 로컬 스토리지에 기본값 설정
+        }
+    }, []);
+
+    // 회사 변경시 로컬 스토리지 업데이트
+    const handleSetCompany = (newCompany: Company) => {
+        setCompany(newCompany);
+        localStorage.setItem('recentCompany', newCompany);
+    };
+
+    const contextValue = useMemo(() => ({ company, setCompany: handleSetCompany }), [company]);
 
     return <CompanyContext.Provider value={contextValue}>{children}</CompanyContext.Provider>;
 };
