4bd45d858b6dfa1b37bd890dfc46eb2cc5de9e31
coldair426 <coldair426@gmail.com>
2025-04-03 15:25:29 +0000

    chore: 캐싱 ㄴㄴ

--- src/app/cafe/cart/[id]/ConfirmClientPage.tsx
+++ src/app/cafe/cart/[id]/ConfirmClientPage.tsx
@@ -47,7 +47,8 @@ export default function OrderConfirmation({ decryptedData, cartId }: ConfirmClie
             const json = await response.json();
             return json.data?.cafeCartItem || [];
         },
-        staleTime: 1000 * 60 * 5,
+        staleTime: 0,
+        refetchOnMount: 'always',
         retry: 1
     });
 
